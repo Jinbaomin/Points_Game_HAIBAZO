711dbf5a1f7e46b496c0b94f33f7a411a1e3aed0
TanMinh-MCN <fctanminh0304@gmail.com>
2025-12-27 18:35:39 +0700

    Updated Task-3 v3.0.2

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
   const boxRef = useRef<HTMLDivElement>(null);
 
   console.log('Updated dev v.1.0.2');
-  console.log('Updated task-3 v3.0.1');
+  console.log('Updated task-3 v3.0.2');
 
   useEffect(() => {
     let interval: any;
